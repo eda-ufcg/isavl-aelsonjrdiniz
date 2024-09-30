020093b5307ea8ffc973f2e2411b9b23b9d794e1
aelsonjrdiniz <aelson.junior.araujo.diniz.cunha@ccc.ufcg.edu.br>
2024-09-30 11:42:14 -0300

    :)

--- src/main/java/BST.java
+++ src/main/java/BST.java
@@ -2,34 +2,105 @@ import java.util.ArrayList;
 import java.util.Deque;
 import java.util.LinkedList;
 
+import java.lang.Math;
+
 public class BST {
 
+    public static void main(String[] args) {
+        BST bst = new BST();
+    	System.out.println(bst.isAVL());
+    	int[] values = new int[]{41,20,11,29,32,65,50,91,72,99};
+
+    	for (int i : values) {
+    	    bst.add(i);
+        }
+
+    	System.out.println(bst.isAVL());
+
+    	bst = new BST();
+    	bst.add(1);
+    	System.out.println(bst.isAVL());
+    	bst.add(2);
+    	System.out.println(bst.isAVL());
+    	bst.add(3);
+    	System.out.println(bst.isAVL());
+
+		bst = new BST();
+    	bst.add(1);
+    	System.out.println(bst.isAVL());
+    	bst.add(3);
+    	System.out.println(bst.isAVL());
+    	bst.add(2);
+    	System.out.println(bst.isAVL());    	
+
+    	bst = new BST();
+    	bst.add(10);
+    	System.out.println(bst.isAVL());
+    	bst.add(5);
+    	System.out.println(bst.isAVL());
+    	bst.add(1);
+    	System.out.println(bst.isAVL());
+
+        System.out.println("Última leva:\n");
+    	bst = new BST();
+    	bst.add(10);
+    	System.out.println(bst.isAVL());
+    	bst.add(5);
+    	System.out.println(bst.isAVL());
+    	bst.add(6);
+    	System.out.println(bst.isAVL());
+
+        bst = new BST();
+    	values = new int[]{90,13,4,8,37,16,24,44,
+    		42,88,51,72,81,84,92};
+    	for (int i : values)
+    		bst.add(i);
+		System.out.println(bst.isAVL());
+
+        bst = new BST();
+		values = new int[]{23, 12, 40, 9, 25, 60, 50};
+    	for (int i : values)
+    		bst.add(i);
+		System.out.println(bst.isAVL());
+    }
     private Node root;
     private int size;
 
     public boolean isAVL() {
-        //TODO: implementar
-        return false;
+        return isAVL(this.root);
+    }
+
+    private boolean isAVL(Node current) {
+    	if (current == null) {
+		return true;
+	} else if (Math.abs(balance(current)) > 1) {
+		return false;
+	} else {
+		return isAVL(current.left) && isAVL(current.right);
+	}
     }
 
     /**
      * Retorna a altura da árvore.
      */
     public int height() {
-        //TODO implementar
-        return -1;
+        return height(this.root);
     }
 
     /**
      * Retorna a altura de um determinado nó. Auxiliar
      * para recursão e para o balance.
      */
+
     private int height(Node node) {
-        return -1;
+       	if (node == null) {
+		return -1;
+	}
+	return 1 + Math.max(height(node.left), height(node.right));
     }
 
     private int balance(Node node) {
-        return -1;
+        return height(node.left) - height(node.right);
     }
 
     /**
